0c5af3aec54e2a6b3fb70223d0f7719b0cdd41bb
Alancb9 <kcarriel@espol.edu.ec>
2023-03-12 19:08:22 -0500

    agregada funcionalidad de buscar pero sin ordenar

--- assets/js/searcher.js
+++ assets/js/searcher.js
@@ -1,7 +1,7 @@
 let loadProducts = async () => {
 
     try {
-        
+
         let jsonUrl = 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.json';
         let xmlUrl = 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.xml';
 
@@ -21,14 +21,14 @@ let loadProducts = async () => {
             let fuente = elementoProducto.getElementsByTagName('src')[0].textContent;
             let tipo = elementoProducto.getElementsByTagName('type')[0].textContent;
 
-            datosProductosXml.push({"name": nombre, "price": precio, "src": fuente, "type": tipo})
-            
+            datosProductosXml.push({ "name": nombre, "price": precio, "src": fuente, "type": tipo })
+
         });
 
         return [datosJson, datosProductosXml];
 
     } catch (error) {
-        
+
         console.error(error);
         return [];
 
@@ -39,8 +39,8 @@ let loadProducts = async () => {
 let recorrerDatos = (menu) => {
 
     let listaPlatos = document.getElementById("seccionPlatos");
-    
-    for (let datosComida of menu){
+
+    for (let datosComida of menu) {
 
         let nuevoPlato = document.createElement("div");
         nuevoPlato.classList.add("col-xl-3", "col-md-6", "mb-xl-0", "mb-4", "mt-4");
@@ -74,39 +74,57 @@ let cargarDatosPlantilla = async () => {
 
     try {
 
-      let datos = await loadProducts();
+        let datos = await loadProducts();
 
-      for (let item of datos) {
-        
-        recorrerDatos(item);
+        for (let item of datos) {
+
+            recorrerDatos(item);
 
 
-      }
+        }
 
     } catch (error) {
 
-      console.error(error);
+        console.error(error);
 
     };
 
 };
 
-// let buscarContenido = () => {
+let buscarContenido = () => {
 
-//     let buscarTexto = document.getElementById("text").value.toLowerCase().trim();
-//     let productos = document.querySelectorAll(".card.card-blog.card-plain");
+    let boton = document.getElementById("filter");
+    boton.addEventListener("click", () => {
 
-//     productos.forEach((producto) => {
+        let buscarTexto = document.getElementById("text").value.toLowerCase().trim();
+        let productos = document.querySelectorAll(".card.card-blog.card-plain");
+
+        productos.forEach((producto) => {
+
+            let nombre = producto.querySelector("h5").textContent.toLowerCase().trim();
+            let tipo = producto.querySelector("p.text-sm").textContent.toLowerCase().trim();
+
+            if (nombre.includes(buscarTexto) || tipo.includes(buscarTexto)) {
+
+                producto.style.display = "block";
+
+            } else {
+
+                producto.style.display = "none";
+
+            }
+
+        });
 
 
+    });
 
-//     });
-
-// };
+};
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    
+document.addEventListener("DOMContentLoaded", function () {
+
     cargarDatosPlantilla();
+    buscarContenido();
 
 });
